e9aa4377afaabcfa04a70b7fb6aa56e530e68d2c
d <d>
2019-06-18 10:06:10 +0800

    fix bug

--- src/main/java/com/github/dingey/mybatis/mapper/MapperMethod.java
+++ src/main/java/com/github/dingey/mybatis/mapper/MapperMethod.java
@@ -2,7 +2,6 @@ package com.github.dingey.mybatis.mapper;
 
 import org.apache.ibatis.builder.annotation.ProviderContext;
 
-import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.HashMap;
@@ -12,19 +11,19 @@ import java.util.Map;
  * @author d
  */
 class MapperMethod {
-	private static final Map<Method, Class<?>> cachedEntity = new HashMap<>();
+	private static final Map<Class<?>, Class<?>> CACHED_ENTITY = new HashMap<>();
 
 	private MapperMethod() {
 	}
 
 	static Class<?> entity(ProviderContext context) {
-		Class<?> aClass = cachedEntity.get(context.getMapperMethod());
+		Class<?> aClass = CACHED_ENTITY.get(context.getMapperType());
 		if (aClass == null) {
 			Type[] genericInterfaces = context.getMapperType().getGenericInterfaces();
 			ParameterizedType pt = (ParameterizedType) genericInterfaces[0];
 			Type type = pt.getActualTypeArguments()[0];
 			aClass = (Class<?>) type;
-			cachedEntity.put(context.getMapperMethod(), aClass);
+			CACHED_ENTITY.put(context.getMapperType(), aClass);
 		}
 		return aClass;
 	}
